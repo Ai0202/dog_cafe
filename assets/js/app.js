8e535ec6ffe589533a332037093f07fdfe6fa491
Atsushi <atsushi88885@gmail.com>
2020-05-22 14:38:50 +0900

    ハンバーガーメニュー展開

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,12 @@ $(function() {
   $('.to-top').on('click', function() {
     $('body, html').animate({ scrollTop: 0 }, 500);
   })
+
+  $('.humberger-menu').on('click', function() {
+    $('nav').addClass('active');
+  })
+
+  $('.nav-close-btn').on('click', function() {
+    $('nav').removeClass('active');
+  })
 });
