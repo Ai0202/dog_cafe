c18250e12d769f320839ec5440d3629db727fe23
Atsushi <atsushi88885@gmail.com>
2020-05-22 15:13:25 +0900

    フォントカラーとタグ修正

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,18 @@
 $(function() {
-  // スムーススクロール
+  // ページトップに戻る
   $('.to-top').on('click', function() {
     $('body, html').animate({ scrollTop: 0 }, 500);
   })
 
+  // ハンバーガーメニューを開く
   $('.humberger-menu').on('click', function() {
-    $('nav').addClass('active');
+    $('#header nav').addClass('active');
   })
 
+  // ハンバーガーメニューを閉じる
   $('.nav-close-btn').on('click', function() {
-    $('nav').removeClass('active');
+    $('#header nav').removeClass('active');
   })
+
+
 });
